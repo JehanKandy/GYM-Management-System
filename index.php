3a616dbef1c3c0f51abcbbbeb01507bc0d7455f7
JehanKandy <jehankandy@gmail.com>
2022-11-23 18:01:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
     include("lib/layouts/header.php");
+    include("lib/layouts/nav.php");
 ?>
+
+<link rel="stylesheet" href="css/style.css">
+
+<img src="images/body1.jpg" alt="body1" class="body-home">
 
 
 <?php 
