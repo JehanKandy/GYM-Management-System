2b5aca52f6b835cc39129263f847cd52a3bbe692
JehanKandy <jehankandy@gmail.com>
2022-11-23 15:33:02 +0530

    Add files via upload

---
+++ lib/layouts/footer_home.php
@@ -0,0 +1,46 @@
+<?php include("header.php"); ?>
+
+<footer class="bg-dark text-white pt-5 pb-4">
+        <div class="container text-center text-md-left">
+            <div class="row text-center text-md-left">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
+                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">Project GYM Workout</h5>
+                    <p>The Project GYM Workout, Develop for educational purposes only.</p>
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
+                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">Partnership</h5>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC Supplements</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">PQR GYM Equipment</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">XYZ Supplements</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">Maleesha GYM Equipment</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">NOP organizations</a></p>
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
+                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">contact details</h5>
+                    <p><i class="fas fa-map-marked-alt"></i> &nbsp;88/A, ABC Street, Kandy, Sri Lanka</p>
+                    <p><i class="fas fa-phone-alt"></i>&nbsp; +94 77 806 4983</p>
+                    <p><i class="fas fa-envelope-open"></i> &nbsp; maleesha123@abc.com</p>
+                </div>
+                <hr class="mb-4">
+                <div class="col-md-7 col-lg-8">
+                    <p>Copyright <i class="far fa-copyright"></i> Project GYM Workout : 2022 All Right Reserved by : 
+                        <a href="#" style="text-decoration:none;" class="text-warning">Maleesha</a></p>
+                </div>
+                <div class="col-md-4 col-lg-3">
+                    <div class="text-center text-md-right">
+                        <ul>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-github-square"></i></a>    
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-linkedin"></i></a>    
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-whatsapp-square"></i></a>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
