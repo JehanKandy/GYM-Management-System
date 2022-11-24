ae4ad27857df3ef9d239e07378d349f5553ed97b
JehanKandy <jehankandy@gmail.com>
2022-11-24 06:10:38 +0530

    Update nav.php

--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -29,7 +29,45 @@
               <a class="nav-link text-warning" href="#">Contact</a>
             </li>
             <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div class="container">
+        <a class="navbar-brand text-warning" href="#">GYM Workout</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link text-warning" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Login</a>
             </li>
           </ul>
         </div>
