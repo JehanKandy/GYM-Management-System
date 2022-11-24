ea1d115fb2892c3a781c1382261b43e94b5e2275
JehanKandy <jehankandy@gmail.com>
2022-11-24 06:10:23 +0530

    Update nav.php

--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
       <div class="container">
-        <a class="navbar-brand" href="#">GYM Workout</a>
+        <a class="navbar-brand text-warning" href="#">GYM Workout</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -17,16 +17,19 @@
           <div class="mx-auto"></div>
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link text-white" href="#">Home</a>
+              <a class="nav-link text-warning" href="#">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-white" href="#">About</a>
+              <a class="nav-link text-warning" href="#">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-white" href="#">Pricing</a>
+              <a class="nav-link text-warning" href="#">Pricing</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-white" href="#">Contact</a>
+              <a class="nav-link text-warning" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Contact</a>
             </li>
           </ul>
         </div>
