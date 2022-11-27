a70acb08df61dcc85f9e477333d544da73f034e6
JehanKandy <jehankandy@gmail.com>
2022-11-27 11:35:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,46 @@
 
 <link rel="stylesheet" href="css/style.css">
 
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div class="container">
+        <a class="navbar-brand text-warning" href="#">GYM Workout</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="docs/about.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
+
 <div class="body-home">
     <div class="topic">
         “Strength does not come <br> from physical capacity. <br> It comes from an indomitable will.”
