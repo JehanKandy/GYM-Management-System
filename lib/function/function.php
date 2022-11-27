64f38a5f339984723db2fc00f916f1d496f42ed2
JehanKandy <jehankandy@gmail.com>
2022-11-28 05:28:37 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -9,5 +9,7 @@
 
         $check_sub_user = "SELECT * FROM geting_touch_tbl WHERE email = '$email'";
         $check_sub_user_result = mysqli_query($con, $check_sub_user);
+        $check_sub_user_nor = mysqli_num_rows($check_sub_user_result);
+
     }
 ?>
