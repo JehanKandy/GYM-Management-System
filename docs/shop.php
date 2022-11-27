d60bf06c9efe6214a3266e90c759cf9058c170b8
JehanKandy <jehankandy@gmail.com>
2022-11-27 12:45:55 +0530

    Update shop.php

--- docs/shop.php
+++ docs/shop.php
@@ -4,6 +4,41 @@
 
 <link rel="stylesheet" href="../css/style.css">
 
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div class="container">
+        <a class="navbar-brand text-warning" href="#">GYM Workout</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="shop.php">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-warning" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
 <?php
     include("../lib/layouts/footer_home.php")
 
