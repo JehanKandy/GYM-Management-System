891a902425759127f30bb86ab2a1b984ab916bb6
JehanKandy <jehankandy@gmail.com>
2022-11-28 05:46:02 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -7,13 +7,25 @@
         $con = Connection();
 
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            return  "Email Not";
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Email Error</strong> &nbsp; Check Again Your Email
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+                </div>";
+        }else{
+            $check_sub_user = "SELECT * FROM geting_touch_tbl WHERE g_email = '$email'";
+            $check_sub_user_result = mysqli_query($con, $check_sub_user);
+            $sub_nor = mysqli_num_rows($check_sub_user_result); 
+
+            if($sub_nor > 0){
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Error</strong> &nbsp; Email Already Exists...!
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                    </div>";
+            }
         }
-
-        $check_sub_user = "SELECT * FROM geting_touch_tbl WHERE g_email = '$email'";
-        $check_sub_user_result = mysqli_query($con, $check_sub_user);
-        $sub_nor = mysqli_num_rows($check_sub_user_result); 
-
-
     }
 ?>
