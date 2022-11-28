16635fdf7f2857da8f8ddb70846e87ef108c6b53
JehanKandy <jehankandy@gmail.com>
2022-11-28 06:34:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-    include("lib/layouts/header.php");
+    include("../layouts/header.php");
 ?>
 
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
       <div class="container">
-        <a class="navbar-brand text-warning" href="#">GYM Workout</a>
+        <a class="navbar-brand text-primary" href="#">GYM Workout</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -23,169 +23,28 @@
           <div class="mx-auto"></div>
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link text-warning" href="index.php">Home</a>
+              <a class="nav-link text-primary" href="../../index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-warning" href="docs/about.php">About</a>
+              <a class="nav-link text-primary" href="../../docs/about.php">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-warning" href="docs/shop.php">Pricing</a>
+              <a class="nav-link text-primary" href="../../docs/shop.php">Pricing</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-warning" href="lib/views/login.php">Login</a>
+              <a class="nav-link text-primary" href="login.php">Login</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
 
-    
-
-<div class="body-home">
-    <div class="topic">
-        “Strength does not come <br> from physical capacity. <br> It comes from an indomitable will.”
-    </div>
-    <div class="person">
-        -Mahatma Gandhi
-    </div>
-</div>
-<div class="brands">
-    <div class="brands-grid">
-        <div class="icon1"></div>
-        <div class="icon2"></div>
-        <div class="icon3"></div>
-        <div class="icon4"></div>
-        <div class="icon5"></div>
-        <div class="icon6"></div>
+<div class="container">
+    <div class="login">
+        <div class="title"><i class="fas fa-user-alt"></i>Login Here</div>
     </div>
 </div>
 
-<div class="home-about">
-    <div class="about-grid">
-        <div class="item1">
-            <div class="img-about">
-                <div class="img-text-about">
-                    GYM WORKOUT
-                </div>
-            </div>
-        </div>
-        <div class="item2">
-            <div class="topic1">
-                Welcome to
-            </div>
-            <div class="topic2">
-                GYM WORKOUT
-            </div>
-            <div class="few-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eius laudantium pariatur corrupti delectus. Omnis animi praesentium eius deserunt totam voluptate vel sapiente a ratione magnam voluptas at, veritatis hic.
-                Incidunt delectus laudantium, cum officia nam commodi magnam distinctio eaque consequatur nesciunt obcaecati labore, atque recusandae quisquam tempore eligendi unde, voluptates dicta. Consectetur cum, labore possimus quod iste suscipit provident.
-                Est velit assumenda et. Voluptas maxime velit quidem, id asperiores, doloremque ratione incidunt et officiis quis commodi repellat maiores ab dignissimos possimus deleniti. Dolor itaque in quasi hic enim cupiditate.
-                Enim, amet. Distinctio officiis est nam ipsam et voluptate molestias natus sint, alias repudiandae, hic iste esse repellendus amet accusamus. Reiciendis corrupti quisquam hic earum asperiores, deserunt alias pariatur unde!
-                Sint inventore alias asperiores velit quidem commodi? Omnis quo nihil debitis. Sapiente molestiae nostrum dolore illo, alias suscipit omnis distinctio doloremque in at consectetur veniam aliquid ab nobis assumenda sunt.
-            </div>
-
-            <a href="#"><button class="more-about-btn">Read More</button></a>
-
-        </div>
-    </div>
-</div>
-
-<div class="team-members">
-    <div class="title">
-        Out Team
-    </div>
-    <div class="body">
-        <div class="team-grid">
-            <div class="item1">
-                <img src="images/person1.jfif" alt="person1" class="team-member-profile">
-                <div class="team-name">
-                    John Wick
-                </div>
-                <div class="team-post">
-                    gym trainer
-                </div>
-                <div class="member-infor">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae labore iusto cum, tempora itaque officia enim at ipsa inventore deleniti voluptates, id voluptatem nihil autem dolores aut ipsam cumque doloribus!
-                    Ducimus provident at nesciunt, corporis repellat tenetur quibusdam minima deleniti laborum consectetur voluptatibus sapiente quas repudiandae id quod quasi obcaecati debitis officiis veniam libero facilis vitae! Neque repudiandae minus dolorum?
-                </div>
-            </div>
-            <div class="item2">
-                <img src="images/person2.jfif" alt="person1" class="team-member-profile">
-                <div class="team-name">
-                    Emma
-                </div>
-                <div class="team-post">
-                    gym trainer
-                </div>
-                <div class="member-infor">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae labore iusto cum, tempora itaque officia enim at ipsa inventore deleniti voluptates, id voluptatem nihil autem dolores aut ipsam cumque doloribus!
-                    Ducimus provident at nesciunt, corporis repellat tenetur quibusdam minima deleniti laborum consectetur voluptatibus sapiente quas repudiandae id quod quasi obcaecati debitis officiis veniam libero facilis vitae! Neque repudiandae minus dolorum?
-                </div>
-            </div>
-            <div class="item3">
-                <img src="images/person3.jfif" alt="person1" class="team-member-profile">
-                <div class="team-name">
-                    James
-                </div>
-                <div class="team-post">
-                    gym trainer
-                </div>
-                <div class="member-infor">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae labore iusto cum, tempora itaque officia enim at ipsa inventore deleniti voluptates, id voluptatem nihil autem dolores aut ipsam cumque doloribus!
-                    Ducimus provident at nesciunt, corporis repellat tenetur quibusdam minima deleniti laborum consectetur voluptatibus sapiente quas repudiandae id quod quasi obcaecati debitis officiis veniam libero facilis vitae! Neque repudiandae minus dolorum?
-                </div>
-            </div>
-            <div class="item4">
-                <img src="images/person4.jfif" alt="person1" class="team-member-profile">
-                <div class="team-name">
-                    William
-                </div>
-                <div class="team-post">
-                    gym trainer
-                </div>
-                <div class="member-infor">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae labore iusto cum, tempora itaque officia enim at ipsa inventore deleniti voluptates, id voluptatem nihil autem dolores aut ipsam cumque doloribus!
-                    Ducimus provident at nesciunt, corporis repellat tenetur quibusdam minima deleniti laborum consectetur voluptatibus sapiente quas repudiandae id quod quasi obcaecati debitis officiis veniam libero facilis vitae! Neque repudiandae minus dolorum?
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="brands">
-    <div class="brands-grid">
-        <div class="icon1"></div>
-        <div class="icon2"></div>
-        <div class="icon3"></div>
-        <div class="icon4"></div>
-        <div class="icon5"></div>
-        <div class="icon6"></div>
-    </div>
-</div>
-
-<div class="subscribe">
-    <div class="title">Getting touch with us</div>
-    <div class="body">
-
-    <?php 
-        include("lib/function/function.php");
-        if(isset($_POST['subscribe'])){
-            $result = subsucribe_user($_POST['sub_email']);
-            echo $result;
-        }
-    ?>
-
-        <form action="<?php echo($_SERVER['PHP_SELF']);?>" method="post">
-            <center><input type="email" name="sub_email" class="email-sub" placeholder="Email" required><br>
-            <input type="submit" value="Subscribe" class="sub-btn" name="subscribe"></center>
-        </form>
-
-        <div class="infor">
-            Getting touch with us using email. we send a email as soon as posiible
-        </div>
-    </div>
-</div>
 <?php 
-    include("lib/layouts/footer_home.php");
+    include("../layouts/footer1.php")    
 ?>
-
-<script src="js/script.js"></script>
