665694c49d551ec7254adf63b5ba6590e810dbec
JehanKandy <jehankandy@gmail.com>
2022-11-28 05:38:00 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1,21 +1,19 @@
 <?php 
     include("config.php");
-    session_start();
     use FTP\Connection;
     
 
     function subsucribe_user($email){
         $con = Connection();
 
-        $check_sub_user = "SELECT * FROM geting_touch_tbl WHERE email = '$email'";
-        $check_sub_user_result = mysqli_query($con, $check_sub_user);
-        $check_sub_user_nor = mysqli_num_rows($check_sub_user_result);
-
-        if($check_sub_user_nor > 0){
-            return "no";
-        }else{
-            return "yes";
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return  "Email Not";
         }
+
+        $check_sub_user = "SELECT * FROM geting_touch_tbl WHERE g_email = '$email'";
+        $check_sub_user_result = mysqli_query($con, $check_sub_user);
+        $sub_nor = mysqli_num_rows($check_sub_user_result); 
+
 
     }
 ?>
