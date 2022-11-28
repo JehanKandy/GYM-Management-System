d49adaa4e0eb7bd04dd98c2782022828c62d1af3
JehanKandy <jehankandy@gmail.com>
2022-11-28 06:19:47 +0530

    Update footer1.php

--- lib/layouts/footer1.php
+++ lib/layouts/footer1.php
@@ -1,3 +1,6 @@
+<?php 
+    include("header.php");
+?>
 <div class="footer1">
-<p>Copyright <i class="far fa-copyright"></i> Project GYM Workout : 2022 All Right Reserved by : 
+    <p>Copyright <i class="far fa-copyright"></i> Project GYM Workout : 2022 All Right Reserved by : <a href="#">Maneesha</a>
 </div>
