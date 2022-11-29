3d0b739356719daccac0ac1935c1976e348516e0
JehanKandy <jehankandy@gmail.com>
2022-11-29 16:37:55 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -49,7 +49,8 @@
                     <?php 
                         include("../function/function.php");
                         if(isset($_POST['register'])){
-                            reg_user($_POST['reg_username'], $_POST['reg_email'], $_POST['reg_pass'], $_POST['reg_cpass']);
+                            $result = reg_user($_POST['reg_username'], $_POST['reg_email'], $_POST['reg_pass'], $_POST['reg_cpass']);
+                            echo $result;
                         }
                     
                     ?>
