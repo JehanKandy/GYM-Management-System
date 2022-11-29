837d5425cc8ba40f10cc4c8a1f5b2e4c614f80dd
JehanKandy <jehankandy@gmail.com>
2022-11-29 16:59:50 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -57,32 +57,30 @@
         $check_user_result = mysqli_query($con, $check_user);
         $check_user_nor = mysqli_num_rows($check_user_result);
 
-        if($check_user_nor > 0){
-            if(empty($username)){
-                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        if(empty($username)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                         <strong>Error : </strong> &nbsp; Username Cannot be Empty.....!
                         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                         <span aria-hidden='true'>&times;</span>
                         </button>
                     </div>";
-            }
-            if(empty($email)){
-                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        }
+        if(empty($email)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                         <strong>Error : </strong> &nbsp; Email Cannot be Empty.....!
                         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                         <span aria-hidden='true'>&times;</span>
                         </button>
                     </div>";
-            }
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                         <strong>Email Error</strong> &nbsp; Check Again Your Email
                         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                         <span aria-hidden='true'>&times;</span>
                         </button>
                     </div>";
-            }
-        }
-            
+         }
     }
+            
 ?>
