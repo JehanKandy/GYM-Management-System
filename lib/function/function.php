f6403529220d2344b1c7ae62dbf20141f8a7408d
JehanKandy <jehankandy@gmail.com>
2022-11-29 16:52:37 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -57,7 +57,7 @@
         $check_user_result = mysqli_query($con, $check_user);
 
         if(empty($username)){
-            return  "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                     <strong>Error : </strong> &nbsp; Username Cannot be Empty.....!
                     <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                     <span aria-hidden='true'>&times;</span>
