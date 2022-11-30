23edb917b0d488f8d5fa24e0b5cf1f0309309c42
JehanKandy <jehankandy@gmail.com>
2022-11-30 14:32:46 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -156,7 +156,8 @@
                     </div>"; 
         }
 
-        $get_user_data = "SELECT * FROM user_tbl WHERE username = '$login_username' && 	user_pass = '$login_pass' && user_status = 0 && is_pending = 1";
+        $get_user_is_padding = "SELECT * FROM user_tbl WHERE username = '$login_username' && 	user_pass = '$login_pass' && user_status = 0 && is_pending = 1";
+        $get_user_is_padding_result = mysqli_query($con, $get_user_is_padding); 
     }
             
 ?>
