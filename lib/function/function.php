d8420dfd46710bd19e0172786d2a39dc74187f3c
JehanKandy <jehankandy@gmail.com>
2022-11-30 16:19:12 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -160,7 +160,7 @@
         $check_user_result = mysqli_query($con, $check_user); 
         $check_user_row = mysqli_fetch_assoc($check_user_result);
 
-        if($login_username != $check_user_row['username']){
+        if($login_username != $check_user_row['u_username']){
             return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                         <strong>Error : </strong> &nbsp; User does not exist.....!
                         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
