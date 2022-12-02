506d16a96b61a5e785249271d571ac039bf33169
JehanKandy <jehankandy@gmail.com>
2022-12-02 20:23:21 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -1,10 +1,8 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
-<link rel="stylesheet" href="../../css/style.css">
 <?php include "../layouts/header.php";?>
 <?php include "../layouts/nav_loged.php";?>
 
 <?php 
-    include("../function/function.php");
     if(empty($_SESSION['LoginSession'])){
         header("location:../views/login.php");
     }
@@ -24,11 +22,10 @@
 			<?php //profile_img();?>
 			<p class="profile-name"><?php //user_id_loged();?></p>
 			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
-			<a href="admin/students.php" class="menu-item"><i class="fas fa-user-graduate"></i>Students</a>
-			<a href="admin/teachers.php" class="menu-item"><i class="fas fa-chalkboard-teacher"></i>Teachers &nbsp; <span class="pending">15</span></a>
-			<a href="admin/quizzes.php" class="menu-item"><i class="fas fa-question-circle"></i>Quizzes</a>
-			<a href="admin/admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin</a>
-			<a href="std_question.php" class="menu-item"><i class="far fa-comments"></i>Chats</a>
+			<a href="admin/teachers.php" class="menu-item"><i class="fas fa-chalkboard-teacher"></i>Members &nbsp; <span class="pending">15</span></a>
+			<a href="admin/admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin  &nbsp; <span class="pending">15</span></a></a>
+			<a href="std_question.php" class="menu-item"><i class="far fa-comments"></i>Prodcuts  &nbsp; <span class="pending">15</span></a></a>
+            <a href="std_question.php" class="menu-item"><i class="far fa-comments"></i>Plans  &nbsp; <span class="pending">15</span></a></a>
 			<a href="admin/my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
 		</nav>
 		
@@ -75,40 +72,6 @@
 						15
 					</div>
 				</div>
-			</div>
-			<div class="last-title">Last Access Users</div>
-			<table class="lastuser-tbl">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Access Time</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Jehankandy</td>
-						<td>jehan@123123123</td>
-						<td>2022/05/22 10:13:45</td>
-					</tr>
-					<tr>
-						<td>Jehankandy</td>
-						<td>jehan@123123123</td>
-						<td>2022/05/22 10:13:45</td>
-					</tr>
-					<tr>
-						<td>Jehankandy</td>
-						<td>jehan@123123123</td>
-						<td>2022/05/22 10:13:45</td>
-					</tr>
-				</tbody>
-			</table>
-			<div class="admin-comment">
-				<div class="title">User Comments</div>
-				<div class="admin-comment-grid">
-				<?php //user_commnets(); ?>
-				</div>
-				<a href="more_comments.php"><button class="comment-add">More Comment</button></a>
 			</div>
 		</div>
 	</main>
