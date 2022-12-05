0983efd47d54c698e29edba35485de53964bea4f
JehanKandy <jehankandy@gmail.com>
2022-12-05 20:21:25 +0530

    Update members.php

--- lib/routes/members.php
+++ lib/routes/members.php
@@ -45,22 +45,22 @@
 			</thead>
 			<tbody>
 				<tr>
-				<th scope="row">1</th>
-				<td>Mark</td>
-				<td>Otto</td>
-				<td>@mdo</td>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
 				</tr>
 				<tr>
-				<th scope="row">2</th>
-				<td>Jacob</td>
-				<td>Thornton</td>
-				<td>@fat</td>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
 				</tr>
 				<tr>
-				<th scope="row">3</th>
-				<td>Larry</td>
-				<td>the Bird</td>
-				<td>@twitter</td>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
 				</tr>
 			</tbody>
 		</table>
