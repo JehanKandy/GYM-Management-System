9d5058157b7c835d4938af4bcd6df45e19580e45
JehanKandy <jehankandy@gmail.com>
2022-12-05 20:36:52 +0530

    Update members.php

--- lib/routes/members.php
+++ lib/routes/members.php
@@ -54,25 +54,29 @@
 					<td>Jehan</td>
 					<td>20010540602233</td>
 					<td><h class="badge badge-success">Active</h></td>
-					<td><button class="btn btn-primary">info</button></td>
-				</tr>
-				<tr>
-					<th scope="row">1</th>
-					<td>Jehan</td>
-					<td>jehan"123</td>
-					<td>Jehan</td>
-					<td>20010540602233</td>
-					<td><h class="badge badge-danger">Deactive</h></td>
-					<td><button class="btn btn-primary">info</button></td>
-				</tr>
-				<tr>
-					<th scope="row">1</th>
-					<td>Jehan</td>
-					<td>jehan"123</td>
-					<td>Jehan</td>
-					<td>20010540602233</td>
-					<td><h class="badge badge-success">Active</h></td>
-					<td><button class="btn btn-primary">info</button></td>
+					<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+						info
+					</button>
+					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							...
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary">Save changes</button>
+						</div>
+						</div>
+					</div>
+					</div>
+					</td>
 				</tr>
 			</tbody>
 		</table>
@@ -80,3 +84,8 @@
 </div>
 
 <script src="../../js/script.js"></script>
+
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
