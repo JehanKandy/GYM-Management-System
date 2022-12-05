58e65ad381b640d39f88a907ae5afde8c922a37e
JehanKandy <jehankandy@gmail.com>
2022-12-05 20:17:47 +0530

    Add files via upload

---
+++ lib/routes/products.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include "../layouts/header.php";?>
+<?php include "../layouts/nav_loged.php";?>
+
+<?php 
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
+    }
+?>
+
+
+
+<div class="app">
+	<div class="menu-toggle">
+		<div class="hamburger">
+			<span></span>
+		</div>
+	</div>
+
+	<aside class="sidebar">
+		<nav class="menu">
+			<?php //profile_img();?>
+			<p class="profile-name"><?php //user_id_loged();?></p>
+			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members &nbsp; <span class="pending">15</span></a>
+			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin  &nbsp; <span class="pending">15</span></a></a>
+			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  &nbsp; <span class="pending">15</span></a></a>
+            <a href="plans.php" class="menu-item"><i class="fas fa-book-reader"></i>Plans  &nbsp; <span class="pending">15</span></a></a>
+			<a href="my_account.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
+		</nav>
+		
+	</aside>
+
+	<main class="content">
+		<h1>Welcome, To Admin Dashboard</h1>
+		<hr>
+		<div class="admin-content">
+			<div class="grid">
+				<div class="admin-item1">
+					<div class="admin-title">
+						<i class="fas fa-user-graduate"></i> &nbsp;  Students<br>
+					</div>
+					<hr class="std-hr">
+					<div class="admin-body">
+						15
+					</div>
+				</div>
+				<div class="admin-item2">
+					<div class="admin-title">
+						<i class="fas fa-chalkboard-teacher"></i> &nbsp;  Teachers<br>
+					</div>
+					<hr class="tea-hr">
+					<div class="admin-body">
+						15
+					</div>
+				</div>
+				<div class="admin-item3">
+					<div class="admin-title">
+						<i class="fas fa-user-tie"></i> &nbsp;  Admins<br>
+					</div>
+					<hr class="admin-hr">
+					<div class="admin-body">
+						15
+					</div>
+				</div>
+				<div class="admin-item4">
+					<div class="admin-title">
+						<i class="fas fa-question-circle"></i> &nbsp;  Quizzes<br>
+					</div>
+					<hr class="qui-hr">
+					<div class="admin-body">
+						15
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
+
+<script src="../../js/script.js"></script>
