be767a4d2fb3c898d50bd695dcbdef794ad0d737
JehanKandy <jehankandy@gmail.com>
2022-12-06 07:16:40 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -521,32 +521,8 @@
                     }                     
 
             $user_data_view .= "
-                        <td><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>
-                            info
-                        </button></td>
-                        </tr>
-                        
-                        <div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                        <div class='modal-dialog' role='document'>
-                            <div class='modal-content'>
-                            <div class='modal-header'>
-                                <h5 class='modal-title' id='exampleModalLabel'>".$user_data_row['user_email']."</h5>
-                                <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
-                                <span aria-hidden='true'>&times;</span>
-                                </button>
-                            </div>
-                            <div class='modal-body'>
-                                ...
-                            </div>
-                            <div class='modal-footer'>
-                                <button type='button'class='btn btn-secondary' data-dismiss='modal'>Close</button>
-                                <button type='button' class='btn btn-primary'>Save changes</button>
-                            </div>
-                            </div>
-                        </div>
-                        </div>                        
-                        
-                
+                            <td><a href='#'><button class='btn btn-primary'>Infor</button></a></td>
+                        </tr>                      
                 ";
                 echo $user_data_view;
         }
