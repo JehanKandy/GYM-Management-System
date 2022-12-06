5e7b0503920156d8ab843e375f843ffd2bb4cd0d
JehanKandy <jehankandy@gmail.com>
2022-12-06 06:23:38 +0530

    Update nav_loged.php

--- lib/layouts/nav_loged.php
+++ lib/layouts/nav_loged.php
@@ -6,13 +6,13 @@
   }
 ?>
 
-
+<link rel="stylesheet" href="../../css/dashboard.css">
 <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
   <button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" style="color:white;" href="#">&nbsp;&nbsp;E-Quiz</a>
+    <a class="navbar-brand" style="color:white;" href="#">&nbsp;&nbsp;GYM Workout - Dashboard</a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
     </ul>
     <span class="navbar-text">
