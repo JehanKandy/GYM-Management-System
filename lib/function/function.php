bea05b8c7bd754a5117ab8bd9b160948074c0383
JehanKandy <jehankandy@gmail.com>
2022-12-06 07:10:22 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -523,7 +523,9 @@
             $user_data_view .= "
                         <td><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>
                             info
-                        </button>
+                        </button></td>
+                        </tr>
+                        
                         <div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
                         <div class='modal-dialog' role='document'>
                             <div class='modal-content'>
@@ -542,9 +544,9 @@
                             </div>
                             </div>
                         </div>
-                        </div>
-                        </td>
-                <tr>
+                        </div>                        
+                        
+                
                 ";
                 echo $user_data_view;
         }
