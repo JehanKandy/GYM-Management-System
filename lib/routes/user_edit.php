227d8a7889c04850bc1b21fe9e352db33d863ca2
JehanKandy <jehankandy@gmail.com>
2022-12-09 20:48:57 +0530

    Add files via upload

---
+++ lib/routes/user_edit.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include "../layouts/header.php";?>
+<?php include "../layouts/nav_loged_user.php";?>
+
+<?php 
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
+    }
+?>
+
