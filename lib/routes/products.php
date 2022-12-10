28472027fcc05c425ce9bfe9b150cb771bb84d7c
JehanKandy <jehankandy@gmail.com>
2022-12-10 10:24:13 +0530

    Update products.php

--- lib/routes/products.php
+++ lib/routes/products.php
@@ -9,6 +9,7 @@
 ?>
 
 
+
 <div class="app">
 	<div class="menu-toggle">
 		<div class="hamburger">
@@ -33,6 +34,7 @@
 	<main class="content">
 		<h1>Products</h1>
 		<hr>
+		<a href="ad_product.php"><button class="btn btn-success"><i class="fas fa-plus"></i> Add Products</button></a>
 
 	</main>
 </div>
