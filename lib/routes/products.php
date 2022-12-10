3d8dd44edc6201d8cc5e4fda18ebab360f622ed8
JehanKandy <jehankandy@gmail.com>
2022-12-10 19:10:03 +0530

    Update products.php

--- lib/routes/products.php
+++ lib/routes/products.php
@@ -18,45 +18,41 @@
 	</div>
 
 	<aside class="sidebar">
-		<nav class="menu">
+	<nav class="menu">
 			<?php //profile_img();?>
 			<p class="profile-name"><?php //user_id_loged();?></p>
 			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
-			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members &nbsp; <span class="pending">15</span></a>
-			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin  &nbsp; <span class="pending">15</span></a></a>
-			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  &nbsp; <span class="pending">15</span></a></a>
-            <a href="plans.php" class="menu-item"><i class="fas fa-book-reader"></i>Plans  &nbsp; <span class="pending">15</span></a></a>
+			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members &nbsp; <span class="count"><?php count_members(); ?></span></a>
+			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin  &nbsp; <span class="count"><?php count_admins(); ?></span></a>
+			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  &nbsp; <span class="count"><?php count_products(); ?></span></a>
+            <a href="plans.php" class="menu-item"><i class="fas fa-book-reader"></i>Plans  &nbsp; <span class="count">15</span></a>
 			<a href="my_account.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
 		</nav>
 		
 	</aside>
 
 	<main class="content">
-		<h1>Products</h1>
+		<h1>All Members</h1>
 		<hr>
-		<span>
-			<a href="add_product.php"><button class="btn btn-success"><i class="fas fa-plus"></i>&nbsp;Add Products</button></a>
-			<a href="all_product.php"><button class="btn btn-primary"><i class="fas fa-gifts"></i>&nbsp;All Products</button></a>
-		</span>
 		<table class="student-tbl">
 			<thead class="thead-dark">
 				<tr>
 					<th scope="col">#</th>
-					<th scope="col">Product</th>
-					<th scope="col">Price</th>
-					<th scope="col">Qty</th>
-					<th scope="col">In Stock</th>
+					<th scope="col">Username</th>
+					<th scope="col">Email</th>
+					<th scope="col">First Name</th>
+					<th scope="col">NIC</th>
 					<th scope="col">Status</th>
-					<th scope="col">Add Date</th>
+					<th scope="col">Is Pending</th>
 					<th scope="col"></th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php any_admin_data(); ?>
+				<?php any_user_data(); ?>
 			</tbody>
 		</table>
-
 	</main>
 </div>
 
 <script src="../../js/script.js"></script>
+
