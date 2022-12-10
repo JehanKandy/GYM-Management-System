611e77d087dfd66d342d763a76d12ff25c4b236f
JehanKandy <jehankandy@gmail.com>
2022-12-10 08:01:30 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -615,7 +615,7 @@
                                 $user_data .="<h3><span class='badge badge-success'>Active</span></h3>
                                 <form action='' method='POST'>
                                 <input type='hidden' name='deactive' value='0'>
-                                <input type='submit' value='Deactivate' name='user_not_pending' class='btn btn-success'>   
+                                <input type='submit' value='Deactivate' name='user_to_pending' class='btn btn-success'>   
                             </form>";
                             }
 
