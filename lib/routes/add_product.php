fd8b3476d45a452e41ca926794bf6cf9c391cd85
JehanKandy <jehankandy@gmail.com>
2022-12-10 10:31:27 +0530

    Update add_product.php

--- lib/routes/add_product.php
+++ lib/routes/add_product.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include "../layouts/header.php";?>
-<?php include "../layouts/nav_loged.php";?>
+<?php include "../layouts/nav_loged_user.php";?>
 
 <?php 
     if(empty($_SESSION['LoginSession'])){
