ae4526bae536f5d0e6a76bc93474d8c7be0cf8a3
JehanKandy <jehankandy@gmail.com>
2022-12-10 18:42:51 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -774,7 +774,9 @@
         $products = "SELECT * FROM shop";
         $products_result = mysqli_query($con, $products);
 
-        
+        while($products_row = mysqli_fetch_assoc($products_result)){
+            $products_data = "";
+        }
 
        
 
