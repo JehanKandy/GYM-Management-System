4ba61d7d9b3d12c02cf2fb2da601277208ee55c5
JehanKandy <jehankandy@gmail.com>
2022-12-11 06:47:58 +0530

    Update edit_product.php

--- lib/routes/edit_product.php
+++ lib/routes/edit_product.php
@@ -10,13 +10,14 @@
 
 <?php 
     if(isset($_POST['update_product_info'])){
-
+        $result = update_product_data($_POST['update_pname'], $_POST['update_pprice'], $_POST['update_pqty'], $_POST['update_pstock']);
+        echo $result;
     }
     if(isset($_POST['product_status_deactive'])){
 
     }
     if(isset($_POST['product_status_active'])){
-        
+
     }
 
 ?>
