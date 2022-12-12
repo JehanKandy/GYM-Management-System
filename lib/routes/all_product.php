74c056164fb882373dc6a2ee67e9b3067a8ab8ef
JehanKandy <jehankandy@gmail.com>
2022-12-12 19:21:54 +0530

    Update all_product.php

--- lib/routes/all_product.php
+++ lib/routes/all_product.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
+<link rel="stylesheet" href="../../css/style.css">
 <?php include "../layouts/header.php";?>
 <?php include "../layouts/nav_loged_user.php";?>
 
@@ -7,3 +8,47 @@
         header("location:../views/login.php");
     }
 ?>
+
+<div class="shop-content">
+    <div class="product-title">Supplements</div>
+    <div class="product-content">
+        <div class="p-grid">
+            <div class="p-item1">
+                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
+                <div class="p-infor">
+                    <div class="p-name">ABC Prodcuts</div>
+                    <div class="qty">1</div>
+                    <div class="p-price">Rs 15,000.00</div>
+                    <div class="p-infor">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex dolorum officiis doloremque. Temporibus quo ipsum unde, rem aperiam beatae nesciunt architecto nulla quasi esse omnis, amet, deserunt tenetur accusamus.</div>
+                </div>
+            </div>
+            
+            <div class="p-item2">
+                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
+                <div class="p-infor">
+                        <div class="p-name">ABC Prodcuts</div>
+                        <div class="qty">1</div>
+                        <div class="p-price">Rs 15,000.00</div>
+                        <div class="p-infor">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex dolorum officiis doloremque. Temporibus quo ipsum unde, rem aperiam beatae nesciunt architecto nulla quasi esse omnis, amet, deserunt tenetur accusamus.</div>
+                </div>
+            </div>
+            <div class="p-item3">
+                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
+                <div class="p-infor">
+                        <div class="p-name">ABC Prodcuts</div>
+                        <div class="qty">1</div>
+                        <div class="p-price">Rs 15,000.00</div>
+                        <div class="p-infor">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex dolorum officiis doloremque. Temporibus quo ipsum unde, rem aperiam beatae nesciunt architecto nulla quasi esse omnis, amet, deserunt tenetur accusamus.</div>
+                </div>
+            </div>
+            <div class="p-item4">
+                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
+                <div class="p-infor">
+                        <div class="p-name">ABC Prodcuts</div>
+                        <div class="qty">1</div>
+                        <div class="p-price">Rs 15,000.00</div>
+                        <div class="p-infor">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex dolorum officiis doloremque. Temporibus quo ipsum unde, rem aperiam beatae nesciunt architecto nulla quasi esse omnis, amet, deserunt tenetur accusamus.</div>
+                </div>
+            </div>
+        </div>
+    </div>
