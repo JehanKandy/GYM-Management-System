881314ad55fcf503433d50df0cd1de437aeab3f6
JehanKandy <jehankandy@gmail.com>
2022-12-13 06:28:08 +0530

    Update add_product.php

--- lib/routes/add_product.php
+++ lib/routes/add_product.php
@@ -45,7 +45,7 @@
                             <input type="text" name="product_stock" id="productStock" class="form-control">
                         </span>
                     </div>
-                    <div class="item-add6">
+                    <div class="item-add5">
                         <input type="submit" value="Add Prodcuct" name='add_prodcut' class="btn btn-success btn-lg btn-block">
                     </div>
                 </div>        
