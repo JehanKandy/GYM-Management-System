0da7493b0eb6c7a50d750f17cd727f0525db2b62
JehanKandy <jehankandy@gmail.com>
2022-12-13 07:01:17 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1079,7 +1079,7 @@
         while($product_row = mysqli_fetch_assoc($select_all_products_result)){
             $product_row_view ="
                 <div class='p-item'>
-                    <center><img src='../../upload/".$product_row['product_img']."' class='supp-img'></center>
+                    <center><img src='../upload/".$product_row['product_img']."' class='supp-img'></center>
                     <div class='p-infor'>
                         <div class='p-name'>".$product_row['p_name']."</div>
                         <div class='qty'>".$product_row['qty']."</div>
