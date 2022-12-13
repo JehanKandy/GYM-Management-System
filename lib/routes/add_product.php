5345f05ed422cc7864ebfac5ddf7c3c3b194ff65
JehanKandy <jehankandy@gmail.com>
2022-12-13 06:24:50 +0530

    Update add_product.php

--- lib/routes/add_product.php
+++ lib/routes/add_product.php
@@ -8,55 +8,24 @@
     }
 ?>
 
+<?php 
+    if(isset($_POST['update_product_info'])){
+        $result = update_product_data($_POST['update_pname'], $_POST['update_pprice'], $_POST['update_pqty'], $_POST['update_pstock']);
+        echo $result;
+    }
+    if(isset($_POST['product_status_deactive'])){
+        $result = product_deactive($_POST['product_deactive']);
+        echo $result;
+    }
+    if(isset($_POST['product_status_active'])){
+        $result = product_active($_POST['product_active']);
+        echo $result;
+    }
+
+?>
 
 <div class="container">
-    <div class="add-product-content">
-        <div class="title">Add Product</div>
-        <div class="body">
-            <?php 
-                if(isset($_POST['add_prodcut'])){
-                    $result = add_product($_POST['product_name'], $_POST['product_price'], $_POST['product_qty'], $_POST['product_stock'], $_POST['p_desc']);
-                    echo $result;
-                }            
-            ?>
-            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
-                <div class="product-add-grid">                    
-                    <div class="item-add1">                        
-                        <span>
-                            Product Name : 
-                            <input type="text" name="product_name" id="productName" class="form-control">
-                        </span>
-                    </div>
-                    <div class="item-add2">
-                        <span>
-                            Product Price : 
-                            <input type="number" name="product_price" id="productPrice" class="form-control">
-                        </span>
-                    </div>
-                    <div class="item-add3">
-                        <span>
-                            Product Qty : 
-                            <input type="text" name="product_qty" id="productQty" class="form-control">
-                        </span>
-                    </div>
-                    <div class="item-add4">
-                        <span>
-                            Product Stock : 
-                            <input type="text" name="product_stock" id="productStock" class="form-control">
-                        </span>
-                    </div>
-                    <div class="item-add5">
-                        <span>
-                            Product Description : 
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="p_desc"></textarea>
-                        </span>
-                    </div>
-                    <div class="item-add6">
-                        <input type="submit" value="Add Prodcuct" name='add_prodcut' class="btn btn-success btn-lg btn-block">
-                    </div>
-                </div>        
-            </form>
-            <a href="products.php"><button class="btn btn-primary btn-lg btn-block" style="margin-top: 20px;">Back</button></a>
-        </div>
+    <div class="edit-product">
+        <?php update_product(); ?>
     </div>
 </div>
