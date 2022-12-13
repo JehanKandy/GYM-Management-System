72a5f4d7de1f14c532da218abd1ff9e269226fea
JehanKandy <jehankandy@gmail.com>
2022-12-13 06:40:04 +0530

    Update all_product.php

--- lib/routes/all_product.php
+++ lib/routes/all_product.php
@@ -12,38 +12,6 @@
 <div class="shop-content">
     <div class="product-content">
         <div class="p-grid">
-            <div class="p-item1">
-                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
-                <div class="p-infor">
-                    <div class="p-name">ABC Prodcuts</div>
-                    <div class="qty">1</div>
-                    <div class="p-price">Rs 15,000.00</div>
-                </div>
-            </div>
-            
-            <div class="p-item2">
-                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
-                <div class="p-infor">
-                        <div class="p-name">ABC Prodcuts</div>
-                        <div class="qty">1</div>
-                        <div class="p-price">Rs 15,000.00</div>
-                </div>
-            </div>
-            <div class="p-item3">
-                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
-                <div class="p-infor">
-                        <div class="p-name">ABC Prodcuts</div>
-                        <div class="qty">1</div>
-                        <div class="p-price">Rs 15,000.00</div>
-                </div>
-            </div>
-            <div class="p-item4">
-                <center><img src="../images/supp1.jfif" alt="" class="supp-img"></center>
-                <div class="p-infor">
-                        <div class="p-name">ABC Prodcuts</div>
-                        <div class="qty">1</div>
-                        <div class="p-price">Rs 15,000.00</div>
-                 </div>
-            </div>
+            <?php all_products_view(); ?>
         </div>
     </div>
