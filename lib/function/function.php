76fd71ee26f9b565733f792a4b0c22dd2314bdbb
JehanKandy <jehankandy@gmail.com>
2022-12-14 15:07:14 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1234,6 +1234,8 @@
             else{
                 $plan_insert = "INSERT INTO plan_tbl(plan_name,plan_status,add_date)VALUES('$plan_name',1,NOW())";
                 $plan_insert_result = mysqli_query($con, $plan_insert);
+
+                header("location:plans.php");
             }
         }
         
