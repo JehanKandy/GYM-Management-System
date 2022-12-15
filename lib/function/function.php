4a5d025890b9b4bccb656e2a9ae526c191b09a17
JehanKandy <jehankandy@gmail.com>
2022-12-15 18:41:03 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1331,10 +1331,10 @@
                         User Type : ";
 
                         if($select_loged_user_data_row['user_type'] == 'admin'){
-                            $login_user_data  ="<span><h3><span class='badge badge-danger'><i class='fas fa-user-tie'></i>Admin</span></h3></span";
+                            $login_user_data  .="<span><h3><span class='badge badge-warning'><i class='fas fa-user-tie'></i>&nbsp;Admin</span></h3></span";
                         }
                         
-                $login_user_data  ="</div>
+                $login_user_data  .="</div>
                 </div>
             </div>
         ";
