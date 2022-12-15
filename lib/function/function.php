8ed9cbd26606a963b6edb2dfd27359f64e6d31ae
JehanKandy <jehankandy@gmail.com>
2022-12-15 21:37:34 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1458,6 +1458,18 @@
                             User Address : 
                             <textarea class='form-control' rows='5' style='resize:none;' name='update_address'>".$select_data_row['user_address']."</textarea>
                         </div>
+                        <div class='p-item6'>
+                            Date of Birth : 
+                            <input type='text' value='".$select_data_row['dob']."' class='form-control' disabled>
+                        </div>
+                        <div class='p-item7'>
+                            NIC/Passport No : 
+                            <input type='text' value='".$select_data_row['nic']."' class='form-control' disabled>
+                        </div>
+                        <div class='p-item8'>
+                            Mobile Number : 
+                            <input type='text' value='".$select_data_row['mobile_no']."' class='form-control' disabled>
+                        </div>
                     </div>
                 </div>
             </form>        
