62e2c501a8493930da19ac255f190081043c75a7
JehanKandy <jehankandy@gmail.com>
2022-12-15 21:38:13 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1460,15 +1460,15 @@
                         </div>
                         <div class='p-item6'>
                             Date of Birth : 
-                            <input type='text' value='".$select_data_row['dob']."' class='form-control' disabled>
+                            <input type='text' value='".$select_data_row['dob']."' name='update_dob' class='form-control' disabled>
                         </div>
                         <div class='p-item7'>
                             NIC/Passport No : 
-                            <input type='text' value='".$select_data_row['nic']."' class='form-control' disabled>
+                            <input type='text' value='".$select_data_row['nic']."' name='update_nic' class='form-control' disabled>
                         </div>
                         <div class='p-item8'>
                             Mobile Number : 
-                            <input type='text' value='".$select_data_row['mobile_no']."' class='form-control' disabled>
+                            <input type='text' value='".$select_data_row['mobile_no']."' name='update_mobile' class='form-control' disabled>
                         </div>
                     </div>
                 </div>
