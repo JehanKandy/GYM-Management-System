7abf31650c5048137a80f0c0388ca763777d7531
JehanKandy <jehankandy@gmail.com>
2022-12-15 22:04:06 +0530

    Update update_profile.php

--- lib/routes/update_profile.php
+++ lib/routes/update_profile.php
@@ -10,10 +10,9 @@
 
 <?php 
     if(isset($_POST['update_profile_info'])){
-        $result = data_update_profile();
+        $result = data_update_profile($_POST['update_username'], $_POST['update_fname'], $_POST['update_lname'], $_POST['update_email'], $_POST['update_address'], $_POST['update_dob'], $_POST['update_nic'], $_POST['update_mobile']);
         echo $result;
     }
-
 ?>
 
 <div class="container">
