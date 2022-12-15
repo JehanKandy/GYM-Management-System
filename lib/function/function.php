8812fcf37625d593a9947acbb505bd6bbaef98ea
JehanKandy <jehankandy@gmail.com>
2022-12-15 18:26:51 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1311,6 +1311,10 @@
                         User Email : 
                         <input type='text' value='".$select_loged_user_data_row['user_email']."' class='form-control' disabled>
                     </div>
+                    <div class='p-item4'>
+                        User Address : 
+                        <textarea class='form-control' rows='3'></textarea>
+                    </div>
                 </div>
             </div>
         ";
