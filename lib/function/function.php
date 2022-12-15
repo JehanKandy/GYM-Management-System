82d83852639e6d1481eb7125440c6b77a73f6fe5
JehanKandy <jehankandy@gmail.com>
2022-12-15 21:53:57 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1496,17 +1496,13 @@
                     }
 
             $update_data  .="</div>
+                    </div>
                 </div>
-            </form>
-            <br><br>
-            ";
+            </form>";
 
             if($select_data_row['user_type'] == 'admin'){
-                $update_data .= "<a href='my_account_admin.php'><button class='btn btn-primary>Back</button></a>";
-            }elseif($select_data_row['user_type'] == 'user'){
-                $update_data .= "<a href=''><button class='btn btn-primary>Back</button></a>";
+                $update_data .="<a href='my_account_admin.php'><button class='btn btn-primary' style='margin-top:30px;'>Back</button></a>";
             }
-
         echo $update_data;
     }
 ?>
