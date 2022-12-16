74c1b45265aaf2cd8559f72199e074e0bde06b5a
JehanKandy <jehankandy@gmail.com>
2022-12-16 14:03:59 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1742,6 +1742,10 @@
         $select_my_activities = "SELECT * FROM user_plan_tbl WHERE user_email = '$login_user'";
         $select_my_activities_result = mysqli_query($con, $select_my_activities);
         $select_my_activities_row = mysqli_fetch_assoc($select_my_activities_result);
+
+        $my_activities = "";
+
+        echo $my_activities;
     }
 ?>
 
