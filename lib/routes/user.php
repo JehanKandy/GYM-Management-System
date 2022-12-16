52369f1ba6972c85e19c294265423a44edc11a73
JehanKandy <jehankandy@gmail.com>
2022-12-16 10:30:38 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -1,9 +1,15 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include "../layouts/header.php";?>
-<?php include "../layouts/nav_loged.php";?>
+<?php include "../layouts/nav_loged_users.php";?>
 
 <?php 
     if(empty($_SESSION['LoginSession'])){
         header("location:../views/login.php");
     }
 ?>
+
+<div class="container">
+    <div class="user-dashboard">
+        hi
+    </div>
+</div>
