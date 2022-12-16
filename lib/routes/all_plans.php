fed9807c01569f2074b532aaa3b7b76791fda366
JehanKandy <jehankandy@gmail.com>
2022-12-16 10:56:02 +0530

    Add files via upload

---
+++ lib/routes/all_plans.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include "../layouts/header.php";?>
+<?php include "../layouts/nav_loged_user.php";?>
+
+<?php 
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
+    }
+?>
+
+<div class="container">
+    <div class="all-plans">
+        hi
+    </div>
+</div>
