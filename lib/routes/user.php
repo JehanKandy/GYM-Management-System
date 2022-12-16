3332ee4e28fdc4f13ec160aafc7eeb0e742beaca
JehanKandy <jehankandy@gmail.com>
2022-12-16 10:41:25 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -10,6 +10,12 @@
 
 <div class="container">
     <div class="user-dashboard">
-        hi
+        <span>
+            <a href=""><button class="btn btn-primary"><i class="fas fa-gifts"></i>Prodcuts</button></a>
+            <a href=""><button class="btn btn-primary">Plans</button></a>
+        </span>
+
+        <br><hr><br>
+        <?php profile_info(); ?>
     </div>
 </div>
