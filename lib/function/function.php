643b75e8cac548e0780bfedcfc221232770c6482
JehanKandy <jehankandy@gmail.com>
2022-12-17 18:30:59 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1693,12 +1693,12 @@
                     <div class='title'>".$plan_data_tbl_row['plan_name']."</div>
                     <hr>";
                 if($plan_data_tbl_row['20p'] == 0){
-                    $plan_view .= "
-                        <div class='complete-plan'>
-                            <div class='text'>
-                                0%
-                            </div>
-                        </div> ";
+                        $plan_view .= "
+                            <div class='complete-plan'>
+                                <div class='text'>
+                                    0%
+                                </div>
+                            </div> ";
                     }
                 if($plan_data_tbl_row['20p'] == 1 && $plan_data_tbl_row['40p'] == 0){
                     $plan_view .= "
@@ -1735,7 +1735,8 @@
                                 100%
                             </div>
                         </div> ";
-           
+                }
+            
 
                 $plan_view .= "</div>
                     <a href='view_my_plan.php?id=".$login_user."'><button class='btn btn-primary' style='margin-top:20px;'>View My Plan</button></a>
