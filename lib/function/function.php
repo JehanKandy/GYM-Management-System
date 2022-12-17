4ea66f23bb98ad0a36442a088e87bfb7fe41d8cf
JehanKandy <jehankandy@gmail.com>
2022-12-17 18:00:05 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1883,11 +1883,15 @@
                     $my_activities .=" <div class='progress'>
                         <div class='progress-bar bg-warning' role='progressbar' style='width: 40%;' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100'>40%</div>
                     </div>";
-                }if($select_my_activities_row['60p'] == 1){
+                }if($select_my_activities_row['60p'] == 1 && $select_my_activities_row['80p'] == 0){
                     $my_activities .=" <div class='progress'>
                         <div class='progress-bar bg-info' role='progressbar' style='width: 60%;' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100'>60%</div>
                     </div>";
-                }                        
+                }if($select_my_activities_row['80p'] == 1){
+                    $my_activities .=" <div class='progress'>
+                        <div class='progress-bar bg-primary' role='progressbar' style='width: 80%;' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100'>80%</div>
+                    </div>";
+                }                             
 
             $my_activities .="
             </div>
